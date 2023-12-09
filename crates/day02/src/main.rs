59a3a4e421009966187c6204bc4f0bf80c9782c7
roycrippen <roycrippen@gmail.com>
2023-12-09 08:38:41 -0600

    shorten

--- crates/day02/src/main.rs
+++ crates/day02/src/main.rs
@@ -3,76 +3,44 @@ fn main() {
         .lines()
         .map(|l| l.to_owned())
         .collect();
-    let games = parse(&ss);
-    let (va, vb) = solve(&games, (12, 13, 14));
+    let (va, vb) = solve(&ss, 12, 13, 14);
     println!("day02-a = {va}"); // 2679
     println!("day02-b = {vb}") // 77607
 }
 
-#[derive(Debug)]
-struct Game {
-    pub id: u32,
-    pub cubes: Vec<(u32, u32, u32)>,
-}
-
-fn solve(games: &Vec<Game>, c: (u32, u32, u32)) -> (u32, u32) {
+fn solve(ss: &[String], nr: u32, ng: u32, nb: u32) -> (u32, u32) {
     let mut va: u32 = 0;
     let mut vb: u32 = 0;
-    for game in games {
+    for (i, s1) in ss.iter().enumerate() {
         let mut a_fail = false;
-        let mut b_max: (u32, u32, u32) = (0, 0, 0);
-        for cube in &game.cubes {
-            b_max = (
-                b_max.0.max(cube.0),
-                b_max.1.max(cube.1),
-                b_max.2.max(cube.2),
-            );
-            if cube.0 > c.0 || cube.1 > c.1 || cube.2 > c.2 {
-                a_fail = true;
+        let mut mr = 0;
+        let mut mg = 0;
+        let mut mb = 0;
+        for s2 in s1.split_once(':').unwrap().1.split(';') {
+            for s3 in s2.split(',') {
+                let vs: Vec<&str> = s3.trim().split(' ').collect();
+                let n = vs[0].parse::<u32>().unwrap();
+                let c = vs[1];
+                match c {
+                    "red" => {
+                        mr = mr.max(n);
+                        a_fail = a_fail || n > nr
+                    }
+                    "green" => {
+                        mg = mg.max(n);
+                        a_fail = a_fail || n > ng
+                    }
+                    _ => {
+                        mb = mb.max(n);
+                        a_fail = a_fail || n > nb
+                    }
+                }
             }
         }
         if !a_fail {
-            va += game.id
+            va += i as u32 + 1;
         }
-        vb += b_max.0 * b_max.1 * b_max.2
+        vb += mr * mg * mb;
     }
     (va, vb)
-}
-
-fn parse(ss: &Vec<String>) -> Vec<Game> {
-    let mut games: Vec<Game> = Vec::with_capacity(ss.len());
-    for (i, s) in ss.iter().enumerate() {
-        let _s = s.split_once(':').unwrap().1;
-        let xss: Vec<Vec<(u32, &str)>> = _s
-            .split(';')
-            .map(|s| {
-                s.split(',')
-                    .map(|s| {
-                        let vs: Vec<&str> = s.trim().split(' ').collect();
-                        let n = vs[0].parse::<u32>().unwrap();
-                        let c = vs[1];
-                        (n, c)
-                    })
-                    .collect::<Vec<(u32, &str)>>()
-            })
-            .collect();
-        let mut game: Game = Game {
-            id: i as u32 + 1,
-            cubes: Vec::new(),
-        };
-        for xs in &xss {
-            let mut cube = (0, 0, 0);
-            for (n, c) in xs {
-                match *c {
-                    "red" => cube.0 = *n,
-                    "green" => cube.1 = *n,
-                    _ => cube.2 = *n,
-                }
-            }
-            game.cubes.push(cube);
-        }
-        // println!("{game:?}");
-        games.push(game);
-    }
-    games
 }
